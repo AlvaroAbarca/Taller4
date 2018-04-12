047a2db6622002061f33d5698f758220647a0afb
AlvaroAbarca <dead.time@live.cl>
2018-04-11 23:22:33 -0300

    Lectura de Archivos lista

---
+++ Taller4.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from openpyxl import Workbook
+from openpyxl import load_workbook
+#wb = Workbook()
+#ws = wb.active()
+listPromL = []
+listPromM = []
+listPromC = []
+
+wb = load_workbook(filename = 'prueba.xlsx')
+sheet_ranges = wb['Sheet1']
+#print(sheet_ranges['J2'].value)
+
+def lectura(list,y): #Se le pasa una lista y una columna
+	for x in range(2,17):
+		asd = str(y)+ str(x)
+		#print(asd)
+		listPromL.append(sheet_ranges[asd].value)
+lectura(listPromL,'J')
+print(listPromL)		
