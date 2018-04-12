37d9fe2cd55ee9a4bf68beec4eec6c7c23c3f2db
AlvaroAbarca <dead.time@live.cl>
2018-04-12 00:31:54 -0300

    Buscar columnas en las que estan los promedios

--- Taller4.py
+++ Taller4.py
@@ -8,14 +8,28 @@ listPromL = []
 listPromM = []
 listPromC = []
 
-wb = load_workbook(filename = 'prueba.xlsx')
-sheet_ranges = wb['Sheet1']
+wb = load_workbook(filename = 'prueba3.xlsx')
+#sheet_ranges = wb['Sheet1']
+ws = wb['Sheet1']
+#print(ws.max_row)
 #print(sheet_ranges['J2'].value)
-
+def ad():
+	lista = ['J','K','L','M','N','P','Q','R']
+	for x in lista:
+		#J K L Region
+		ww = ws[x+'1'].value
+		#print(ww[0:4])
+		if (ww[0:4] == "prom"):
+			varInicio = x
+			print (varInicio)
+		#L M N Provincia
+		#P Q R Comunaa Publica
+	#return x
 def lectura(list,y): #Se le pasa una lista y una columna
-	for x in range(2,17):
+	for x in range(2,ws.max_row+1):
 		asd = str(y)+ str(x)
 		#print(asd)
-		list.append(sheet_ranges[asd].value)
-lectura(listPromL,'J')
-print(listPromL)		
+		list.append(ws[asd].value)
+#lectura(listPromL,'J')
+#print(listPromL)	
+ad()	
